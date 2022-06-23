fcb267ca70f5c992685342ecd37d7963d1fdd9f7
Juho Pietilä <juho.pietila@fmi.fi>
2022-06-23 15:11:35 +0300

    data used

--- hands-on-GP/Gramacy_GP.R
+++ hands-on-GP/Gramacy_GP.R
@@ -1,3 +1,5 @@
+##### 5.1 Gaussian process prior #####
+
 n <- 100
 X <- matrix(seq(0, 10, length=n), ncol=1)
 
@@ -15,6 +17,8 @@ plot(X, Y, type="l")
 Y <- rmvnorm(5, sigma=Sigma)
 matplot(X, t(Y), type="l", ylab="Y")
 abline(h = c(-2,2), col = 'red', type = "l", lty = 2, lwd = 3)
+
+##### 5.1.1 Gaussian process posterior #####
 
 n <- 8
 X <- matrix(seq(0,2*pi,length=n), ncol=1) #training points
@@ -46,4 +50,330 @@ lines(XX, mup, lwd=2)
 lines(XX, q1, lwd=2, lty=2, col=2)
 lines(XX, q2, lwd=2, lty=2, col=2)
 
+##### trying to fit in the same model using laGP #####
 
+gpi <- newGP(X, y, d=0.1, g=0, dK=TRUE)
+#mle <- mleGP(gpi, param="both", tmin=c(eps, eps), tmax=c(10, var(y)))
+mle <- mleGP(gpi, param = c("d", "g"))
+p <- predGP(gpi, XX)
+YY <- rmvnorm(100, p$mean, p$Sigma)
+q1 <- p$mean + qnorm(0.05, 0, sqrt(diag(p$Sigma)))
+q2 <- p$mean + qnorm(0.95, 0, sqrt(diag(p$Sigma)))
+
+matplot(XX, t(YY), type="l", col="gray", lty=1, xlab="x", ylab="y")
+points(X, y, pch=20, cex=2)
+lines(XX, sin(XX), col="blue")
+lines(XX, p$mean, lwd=2)
+lines(XX, q1, lwd=2, lty=2, col=2)
+lines(XX, q2, lwd=2, lty=2, col=2)
+
+deleteGP(gpi)
+
+##### 5.1.2 Higher dimension #####
+
+nx <- 20
+x <- seq(0, 2, length=nx)
+X <- expand.grid(x, x)
+
+D <- distance(X)
+Sigma <- exp(-D) + diag(eps, nrow(X))
+
+Y <- rmvnorm(2, sigma=Sigma)
+
+par(mfrow=c(1,2))
+persp(x, x, matrix(Y[1,], ncol=nx), theta=-30, phi=30, xlab="x1",
+      ylab="x2", zlab="y")
+persp(x, x, matrix(Y[2,], ncol=nx), theta=-30, phi=30, xlab="x1",
+      ylab="x2", zlab="y")
+
+library(lhs)
+X <- randomLHS(40, 2)
+X[,1] <- (X[,1] - 0.5)*6 + 1
+X[,2] <- (X[,2] - 0.5)*6 + 1
+y <- X[,1]*exp(-X[,1]^2 - X[,2]^2)
+
+xx <- seq(-2, 4, length=40)
+XX <- expand.grid(xx, xx)
+
+D <- distance(X)
+Sigma <- exp(-D)
+DXX <- distance(XX)
+SXX <- exp(-DXX) + diag(eps, ncol(DXX))
+DX <- distance(XX, X)
+SX <- exp(-DX)
+
+Si <- solve(Sigma)
+mup <- SX %*% Si %*% y
+Sigmap <- SXX - SX %*% Si %*% t(SX)
+
+yy <- rmvnorm(2, mean = mup, sigma = Sigmap)
+
+sdp <- sqrt(diag(Sigmap)) # posterior standard deviations
+
+par(mfrow=c(1,2))
+cols <- heat.colors(128)
+image(xx, xx, matrix(mup, ncol=length(xx)), xlab="x1", ylab="x2", col=cols)
+points(X[,1], X[,2])
+image(xx, xx, matrix(sdp, ncol=length(xx)), xlab="x1", ylab="x2", col=cols)
+points(X[,1], X[,2])
+
+par(mfrow=c(1,1))
+persp(xx, xx, matrix(mup, ncol=40), theta=-30, phi=30, xlab="x1",
+      ylab="x2", zlab="y")
+
+##### 5.2 GP Hyperparameters #####
+
+n <- 100
+X <- matrix(seq(0, 10, length=n), ncol=1)
+D <- distance(X)
+C <- exp(-D) + diag(eps, n)
+tau2 <- 25
+Y <- rmvnorm(10, sigma=tau2*C)
+matplot(X, t(Y), type="l")
+abline(h = c(-10,10))
+
+#example of why do we need hypers:
+
+n <- 8
+X <- matrix(seq(0, 2*pi, length=n), ncol=1)
+y <- 5*sin(X) #amplitude of 5
+
+plot(X,y)
+
+D <- distance(X)
+Sigma <- exp(-D)
+XX <- matrix(seq(-0.5, 2*pi + 0.5, length=100), ncol=1)
+DXX <- distance(XX)
+SXX <- exp(-DXX) + diag(eps, ncol(DXX))
+DX <- distance(XX, X)
+SX <- exp(-DX)
+Si <- solve(Sigma);
+mup <- SX %*% Si %*% y
+Sigmap <- SXX - SX %*% Si %*% t(SX)
+
+YY <- rmvnorm(100, mup, Sigmap)
+q1 <- mup + qnorm(0.05, 0, sqrt(diag(Sigmap)))
+q2 <- mup + qnorm(0.95, 0, sqrt(diag(Sigmap)))
+matplot(XX, t(YY), type="l", col="gray", lty=1, xlab="x", ylab="y")
+points(X, y, pch=20, cex=2)
+lines(XX, mup, lwd=2)
+lines(XX, 5*sin(XX), col="blue")
+lines(XX, q1, lwd=2, lty=2, col=2)
+lines(XX, q2, lwd=2, lty=2, col=2)
+
+CX <- SX
+Ci <- Si
+CXX <- SXX
+tau2hat <- drop(t(y) %*% Ci %*% y / length(y))
+
+mup2 <- CX %*% Ci %*% y
+Sigmap2 <- tau2hat*(CXX - CX %*% Ci %*% t(CX))
+
+YY <- rmvnorm(100, mup2, Sigmap2)
+q1 <- mup + qnorm(0.05, 0, sqrt(diag(Sigmap2)))
+q2 <- mup + qnorm(0.95, 0, sqrt(diag(Sigmap2)))
+
+matplot(XX, t(YY), type="l", col="gray", lty=1, xlab="x", ylab="y")
+points(X, y, pch=20, cex=2)
+lines(XX, mup, lwd=2)
+lines(XX, 5*sin(XX), col="blue")
+lines(XX, q1, lwd=2, lty=2, col=2); lines(XX, q2, lwd=2, lty=2, col=2)
+
+score <- function(Y, mu, Sigma, mah=FALSE)
+{
+  Ymmu <- Y - mu
+  Sigmai <- solve(Sigma)
+  mahdist <- t(Ymmu) %*% Sigmai %*% Ymmu
+  if(mah) return(sqrt(mahdist))
+  return (- determinant(Sigma, logarithm=TRUE)$modulus - mahdist)
+}
+
+Ytrue <- 5*sin(XX)
+df <- data.frame(score(Ytrue, mup, Sigmap, mah=TRUE),
+                 score(Ytrue, mup2, Sigmap2, mah=TRUE))
+colnames(df) <- c("tau2=1", "tau2hat")
+df
+
+### optimizing log-likelihood with noise (nugget)
+nlg <- function(g, D, Y)
+{
+  n <- length(Y)
+  K <- exp(-D) + diag(g, n)
+  Ki <- solve(K)
+  ldetK <- determinant(K, logarithm=TRUE)$modulus
+  ll <- - (n/2)*log(t(Y) %*% Ki %*% Y) - (1/2)*ldetK
+  counter <<- counter + 1
+  return(-ll)
+}
+
+X <- matrix(seq(0, 2*pi, length=n), ncol=1)
+X <- rbind(X, X)
+n <- nrow(X)
+y <- 5*sin(X) + rnorm(n, sd=1)
+D <- distance(X)
+
+counter <- 0
+g <- optimize(nlg, interval=c(eps, var(y)), D=D, Y=y)$minimum
+g
+
+K <- exp(-D) + diag(g, n)
+Ki <- solve(K)
+tau2hat <- drop(t(y) %*% Ki %*% y / n)
+c(tau=sqrt(tau2hat), sigma=sqrt(tau2hat*g)) #sigma = noise?
+
+plot(X,y)
+
+DX <- distance(XX, X)
+KX <- exp(-DX)
+KXX <- exp(-DXX) + diag(g, nrow(DXX)) ## add some noise on test points?
+
+mup <- KX %*% Ki %*% y
+Sigmap <- tau2hat*(KXX - KX %*% Ki %*% t(KX))
+q1 <- mup + qnorm(0.05, 0, sqrt(diag(Sigmap)))
+q2 <- mup + qnorm(0.95, 0, sqrt(diag(Sigmap)))
+
+Sigma.int <- tau2hat*(exp(-DXX) + diag(eps, nrow(DXX)) #difference between Sigmap?
+                      - KX %*% Ki %*% t(KX))
+YY <- rmvnorm(100, mup, Sigmap)
+YY <- rmvnorm(100, mup, Sigma.int)
+
+
+matplot(XX, t(YY), type="l", lty=1, col="gray", xlab="x", ylab="y")
+points(X, y, pch=20, cex=2)
+lines(XX, mup, lwd=2)
+lines(XX, 5*sin(XX), col="blue")
+lines(XX, q1, lwd=2, lty=2, col=2)
+lines(XX, q2, lwd=2, lty=2, col=2)
+
+nl <- function(par, D, Y)
+{
+  theta <- par[1] ## change 1
+  g <- par[2]
+  n <- length(Y)
+  K <- exp(-D/theta) + diag(g, n) ## change 2
+  Ki <- solve(K)
+  ldetK <- determinant(K, logarithm=TRUE)$modulus
+  ll <- - (n/2)*log(t(Y) %*% Ki %*% Y) - (1/2)*ldetK
+  counter <<- counter + 1
+  return(-ll)
+}
+
+library(lhs)
+X2 <- randomLHS(40, 2)
+X2 <- rbind(X2, X2)
+X2[,1] <- (X2[,1] - 0.5)*6 + 1
+X2[,2] <- (X2[,2] - 0.5)*6 + 1
+y2 <- X2[,1]*exp(-X2[,1]^2 - X2[,2]^2) + rnorm(nrow(X2), sd=0.01)
+
+D <- distance(X2)
+counter <- 0
+out <- optim(c(0.1, 0.1*var(y2)), nl, method="L-BFGS-B", lower=eps,
+             upper=c(10, var(y2)), D=D, Y=y2)
+out$par
+
+gradnl <- function(par, D, Y)
+{
+  ## extract parameters
+  theta <- par[1]
+  g <- par[2]
+  ## calculate covariance quantities from data and parameters
+  n <- length(Y)
+  K <- exp(-D/theta) + diag(g, n)
+  Ki <- solve(K)
+  dotK <- K*D/theta^2
+  KiY <- Ki %*% Y
+  ## theta component
+  dlltheta <- (n/2) * t(KiY) %*% dotK %*% KiY / (t(Y) %*% KiY) -
+    (1/2)*sum(diag(Ki %*% dotK))
+  ## g component
+  dllg <- (n/2) * t(KiY) %*% KiY / (t(Y) %*% KiY) - (1/2)*sum(diag(Ki))
+  ## combine the components into a gradient vector
+  return(-c(dlltheta, dllg))
+}
+
+counter <- 0
+outg <- optim(c(0.1, 0.1*var(y2)), nl, gradnl, method="L-BFGS-B",
+              lower=eps, upper=c(10, var(y2)), D=D, Y=y2)
+rbind(grad=outg$par, brute=out$par)
+
+outg$par #optimized theta = 1, g = 0.006
+
+K <- exp(- D/outg$par[1]) + diag(outg$par[2], nrow(X2))
+Ki <- solve(K)
+tau2hat <- drop(t(y2) %*% Ki %*% y2 / nrow(X2))
+
+gn <- 40
+xx <- seq(-2, 4, length=gn)
+XX <- expand.grid(xx, xx)
+DXX <- distance(XX)
+KXX <- exp(-DXX/outg$par[1]) + diag(outg$par[2], ncol(DXX))
+DX <- distance(XX, X2)
+KX <- exp(-DX/outg$par[1])
+
+mup <- KX %*% Ki %*% y2
+Sigmap <- tau2hat*(KXX - KX %*% Ki %*% t(KX))
+sdp <- sqrt(diag(Sigmap))
+
+par(mfrow=c(1,2))
+image(xx, xx, matrix(mup, ncol=gn), main="mean", xlab="x1",
+      ylab="x2", col=cols)
+points(X2)
+image(xx, xx, matrix(sdp, ncol=gn), main="sd", xlab="x1",
+      ylab="x2", col=cols)
+points(X2)
+
+### multi-dimensional example!!!
+
+fried <- function(n=50, m=6)
+{
+  if(m < 5) stop("must have at least 5 cols")
+  X <- randomLHS(n, m)
+  Ytrue <- 10*sin(pi*X[,1]*X[,2]) + 20*(X[,3] - 0.5)^2 + 10*X[,4] + 5*X[,5]
+  Y <- Ytrue + rnorm(n, 0, 1)
+  return(data.frame(X, Y, Ytrue))
+}
+
+m <- 7
+n <- 200
+nprime <- 1000
+data <- fried(n + nprime, m)
+X <- as.matrix(data[1:n,1:m])
+y <- drop(data$Y[1:n])
+XX <- as.matrix(data[(n + 1):nprime,1:m])
+yy <- drop(data$Y[(n + 1):nprime])
+yytrue <- drop(data$Ytrue[(n + 1):nprime])
+
+D <- distance(X)
+out <- optim(c(0.1, 0.1*var(y)), nl, gradnl, method="L-BFGS-B", lower=eps,
+             upper=c(10, var(y)), D=D, Y=y)
+out$par
+
+K <- exp(- D/out$par[1]) + diag(out$par[2], nrow(D))
+Ki <- solve(K)
+tau2hat <- drop(t(y) %*% Ki %*% y / nrow(D))
+
+DXX <- distance(XX)
+KXX <- exp(-DXX/out$par[1]) + diag(out$par[2], ncol(DXX))
+DX <- distance(XX, X)
+KX <- exp(-DX/out$par[1])
+
+mup <- KX %*% Ki %*% y
+Sigmap <- tau2hat*(KXX - KX %*% Ki %*% t(KX))
+
+### package implementations
+library(laGP)
+tic <- proc.time()[3]
+gpi <- newGPsep(X, y, d=0.1, g=0.1*var(y), dK=TRUE)
+gpi2 <- newGP(X, y, d=0.1, g=0.1*var(y), dK = TRUE)
+
+mle <- mleGPsep(gpi, param="both", tmin=c(eps, eps), tmax=c(10, var(y)))
+mle2 <- mleGP(gpi2, param = "both", tmin = c(eps,eps), tmax=c(10, var(y)))
+toc <- proc.time()[3]
+
+thetahat <- rbind(grad=outg$par, brute=out$par, laGP=mle$theta)
+colnames(thetahat) <- c(paste0("d", 1:ncol(X)), "g")
+thetahat
+
+p <- predGPsep(gpi, XX)
+
+deleteGPsep(gpi)
