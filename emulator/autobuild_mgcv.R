a9ff2fd8522f4ba0ba471fe8448c9d67db7c3e87
Juho Pietilä <juho.pietila@fmi.fi>
2022-08-15 16:02:41 +0300

    deleting unrelevant examples

--- emulator/autobuild_mgcv.R
+++ emulator/autobuild_mgcv.R
@@ -1,8 +1,8 @@
 library(mgcv)
 
 ### assume that data is your design matrix and var is the name of your response variabe (e.g. "likelihood" in our case)
-### this model will build and return you a gam model with n_interaction interaction terms (default = 0)
-### you can also tune your basis (cr = cubic regression as a default)
+### this function will build and return you a gam model with n_interaction interaction terms (default = 0)
+### you can also tune your basis (cr = cubic regression as a default) or set type = "gam" to fit "normal gam"
 
 build_mgcv <- function(data, var, bs = "cr", type = "bam", n_interactions = 0) {
   preds <- colnames(data)
@@ -62,11 +62,6 @@ build_mgcv <- function(data, var, bs = "cr", type = "bam", n_interactions = 0) {
       gam(as.formula(final_formula), data = data, method = "REML")
     }
     
-    # print(final_formula)
-    # print(which.min(unlist(aics[-10])))
-    # print(order(unlist(aics), decreasing = FALSE))
-    # print(id)
-    # print(aics)
     
   } else { # if n_interactions == 0
     final_mod <- if(type=="bam") {
@@ -77,44 +72,3 @@ build_mgcv <- function(data, var, bs = "cr", type = "bam", n_interactions = 0) {
   }
   return(final_mod)
 }
-
-
-# 
-# tic <- proc.time()
-#build_mgcv(training_set, "likelihood", "cr","bam",n_interactions = 3)
-# toc <- proc.time()
-# print((toc-tic)[3])
-
-#build_mgcv(training_set, "likelihood", "bs", "bam", 4)
-
-# Assuming D_MAT is your design matrix with 6 columns with names of the parameters below and a last column called "llik"
-# preds <- c("som_respiration_rate", "psnTOpt", "wueConst", "leafGrowth", "rdConst")
-# 
-# n  <- length(preds)
-# id <- unlist(lapply(1:2, function(i) combn(1:n,i,simplify=FALSE)), recursive=FALSE)
-# # also see 
-# #id <- unlist(lapply(1:3, function(i) combn(1:n,i,simplify=FALSE)), recursive=FALSE)
-# 
-# var <- "likelihood"
-# # generate formulas
-# Formulas <- sapply(id, function(i){
-#   if(length(i) == 1){
-#     foo <- paste(paste(preds[i],collapse="*"))
-#   }else{
-#     foo <- paste(paste(preds[i],collapse="+"),"+",paste(paste(preds[i],collapse=":")))
-#   }
-#   return(paste(var,"~ (", foo, ")"))
-# }
-# )
-# 
-# fits <- vector("list", length(Formulas))
-# for(i in seq_along(Formulas)){
-#   fits[[i]] <- lm(as.formula(Formulas[i]), data=D_MAT)
-# }
-# #then you can loop over fits and check some properties, is it significant which has the most deviance explained, least penalized etc.
-# 
-# Formulas
-
-# mod <- build_mgcv(training_set, "likelihood", "cr", "bam", 3)
-# gam.check(mod)
-
